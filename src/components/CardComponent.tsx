0ae1dfa7a1064641e91fd057e19f378c33836f24
Abhinav Singh <riyanshi98765riyanshi4321@gmail.com>
2024-08-22 04:08:29 +0530

    last moment changes 2

--- src/components/CardComponent.tsx
+++
@@ -1,7 +0,0 @@
-export const CardComponent = ({title, description , icon} : {title : string , description : string , icon : string}) => {
-    return (
-        <div className="">
-
-        </div>
-    );
-}
